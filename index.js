e1659d30589b8f600364621a002b36763e08b18c
Vitor Azevedo <vigsoft410@gmail.com>
2020-12-04 21:06:33 -0300

    :bug: & :rotating_light: Resolvendo erro e bug no gifsticker, e refatorando código

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
 const venom = require('venom-bot');
 const fs = require('fs');
 const mime = require('mime-types');
 const ffmpeg = require('fluent-ffmpeg');
-const { setTimeout } = require('timers');
-venom
-  .create()
-  .then((client) => start(client));
 
-async function start(client)  {
+venom
+    .create()
+    .then((client) => start(client));
+
+async function start(client) {
   client.onMessage(async (message) => {
+    // Mensagens personalizadas layout
+    const customMessage = (content) => {
+      client
+          .reply(message.from, `*[BOT] ${content}`, message.id)
+          .then(() => {
+            console.log(`${message.sender.pushname}:`, message.body);
+          })
+          .catch((erro) => {
+            console.error('Erro ao enviar mensagem: ', erro);
+          });
+    };
 
     // Help menssage
     if (message.body === '!help') {
       client
-        .sendText(`${message.from}`, 
-        '*[BOT]* \n \n  Para criar uma figurinha digite *!sticker* \n \n Para criar uma figurinha animada digite *!gifsticker*'
-        )
-        .then(() => {
-          console.log(`${message.sender.pushname}:`, message.body);
-        })
-        .catch((erro) => {
-          console.error('Erro ao enviar mensagem: ', erro);
-        });
-    }
-
-    if (message.body === 'Renan') {
-      client
-        .sendText(`${message.from}`, '*[BOT] Renan é o caralho.*')
-        .then(() => {
-          console.log(`${message.sender.pushname}:`, message.body);
-        })
-        .catch((erro) => {
-          console.error('Erro ao enviar mensagem: ', erro);
-        });
+          .sendText(`${message.from}`,
+              '*[BOT]* \n \n  Para criar uma figurinha digite *!sticker* \n \n Para criar uma figurinha animada digite *!gifsticker*',
+          )
+          .then(() => {
+            console.log(`${message.sender.pushname}:`, message.body);
+          })
+          .catch((erro) => {
+            console.error('Erro ao enviar mensagem: ', erro);
+          });
     }
 
     // Image sticker
@@ -43,55 +45,89 @@ async function start(client)  {
         await fs.writeFile(fileName, buffer, (err) => {
           if (err) throw err;
           client
-          .sendImageAsSticker(message.chatId, fileName)
-          .then(() => {
-            console.log(`${message.sender.pushname}:`, 'image');
-          })
+              .sendImageAsSticker(message.chatId, fileName)
+              .then(() => {
+                console.log(`${message.sender.pushname}:`, 'image');
+              });
         });
       }
     }
 
-    // Gif sticker
     if (message.caption === '!gifsticker') {
       const buffer = await client.decryptFile(message);
+
       const videoFile = `./files/img-gif-sticker_${message.sender.pushname}+${message.sender.id}.${mime.extension(message.mimetype)}`;
       const gifFile = `./files/img-gif-sticker_tmp_${message.sender.pushname}+${message.sender.id}.${mime.extension('image/gif')}`;
 
+      // Verificando se existe arquivos antigos e removendo-os
+      if (fs.existsSync(videoFile)) {
+        try {
+          fs.unlinkSync(videoFile);
+        } catch (err) {
+          console.log('WARNING: unlinkSync', err);
+        }
+      }
+      if (fs.existsSync(gifFile)) {
+        try {
+          fs.unlinkSync(gifFile);
+        } catch (err) {
+          console.log('WARNING: unlinkSync', err);
+        }
+      }
+
+      // Enviando gifsticker
       const messageSend = () => {
         client
-        .sendImageAsStickerGif(message.chatId, gifFile)
-        .then((result) => {
-          console.log('Enviado para: ', result.to.formattedName);
-        })
-      }
+            .sendImageAsStickerGif(message.chatId, gifFile)
+            .then((result) => {
+              console.log('Result: ', result);
+            });
+      };
 
+      // Convertendo o video(input) para gif(output)
       const convert = (input, output) => {
         ffmpeg(input)
-          .output(output)
-          .size('256x256')
-          .on('end', () => {                    
-            console.log('convert: Convertendo video pra gif');
-          }).on('error', (err) => {
-            console.log('convert_INSIDE_FUNCTION_ERROR: ', err);
-          }).run();
-      }
+            .output(output)
+            .size('256x256')
+            .run();
+      };
 
-      // Escrevendo gif com o Buffer
+      // Salvando o gif
       await fs.writeFileSync(gifFile, buffer, (err) => {
-        if(err) console.log('write_gif_ERROR:', err);
+        if (err) throw err;
       });
 
-      // Escrevendo mp4 com o Buffer
-      fs.writeFile(videoFile, buffer, async (err) => {
-        if(err) console.log('write_mp4_ERROR:', err);
-
-        // Convertendo
-        await convert(videoFile, gifFile).catch((err) => { if(err) console.log('Convert_ERROR:', err) });
-
-        // Esperando 3 segundos e enviando o gif
-        setTimeout(messageSend, 3000);
+      // Salvando o video
+      fs.writeFile(videoFile, buffer, (err) => {
+        if (err) throw err;
+        try {
+          convert(videoFile, gifFile);
+        } catch (err) {
+          console.log('convert_TRY_INSIDE_FUNCTION_ERROR: ', err);
+        }
+        setTimeout(messageSend, 500);
       });
-
     }
-	});
+
+    // Mensagens personalizadas
+    if (message.body === 'Renan') {
+      customMessage('Renan é o caralho.');
+    }
+
+    if (message.body === 'Jeff') {
+      customMessage('Jeff é a nordestina safada do grupo.');
+    }
+
+    if (message.body === 'QI') {
+      customMessage('Seu QI de Leo do caralho.');
+    }
+
+    if (message.body === 'Léo' || message.body === 'Leo') {
+      customMessage('Léo é o gordo todo fodido do grupo.\n Ele é tão gordo que o DDD muda, quando ele troca o celular de bolso');
+    }
+
+    if (message.body === 'Rafael') {
+      customMessage('Rafael ou tê oitocentos, é o irmão afeminado e fracassado do Renan.');
+    }
+  });
 }
